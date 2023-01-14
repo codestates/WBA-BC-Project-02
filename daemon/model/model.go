35a010e615c7af6200d35c4598a4b9a2c346de7c
steve <ionc635@naver.com>
2023-01-14 19:21:38 +0900

    add: mongoDB 연결 및 config 설정

---
+++ daemon/model/model.go
@@ -0,0 +1,29 @@
+package model
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Model struct {
+	client   *mongo.Client
+	colBlock *mongo.Collection
+}
+
+func NewModel(mgUrl string) (*Model, error) {
+	r := &Model{}
+
+	var err error
+	if r.client, err = mongo.Connect(context.Background(), options.Client().ApplyURI(mgUrl)); err != nil {
+		return nil, err
+	} else if err := r.client.Ping(context.Background(), nil); err != nil {
+		return nil, err
+	} else {
+		db := r.client.Database("daemon")
+		r.colBlock = db.Collection("transaction")
+	}
+
+	return r, nil
+}
