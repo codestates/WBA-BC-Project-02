94b1a83ddbe3cf054ff8cadceed4f008662ba2b1
Hooeats <hooneats@naver.com>
2023-01-16 21:41:19 +0900

    Redis 추상화 진행 후 인터페이스인 cacher 를 커밋안해서 커밋 및 구조 개선, refresh token 은 httpOnly 쿠키에 담기로 했음으로 내려줄때 response body 에서는 제외

---
+++ was/common/cache/cacher.go
@@ -0,0 +1,16 @@
+package cache
+
+import "time"
+
+var AppCacher Cacher
+
+type Cacher interface {
+	Cache(key string, data any, du time.Duration) error
+	Update(key string, data any, du time.Duration) error
+	Delete(keys ...string) error
+	Get(key string, t any) (any, error)
+}
+
+func SetAppCacher(cache Cacher) {
+	AppCacher = cache
+}
