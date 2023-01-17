37965be6fefc45cd76338eab579858d05609f451
steve <ionc635@naver.com>
2023-01-17 16:29:17 +0900

    add: draco listener custom transfer 추가

--- daemon/subscribe/draco_listener.go
+++ daemon/subscribe/draco_listener.go
@@ -12,6 +12,7 @@ import (
 	draco "github.com/codestates/WBA-BC-Project-02/contracts/draco"
 	"github.com/codestates/WBA-BC-Project-02/daemon/model"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts/abi"
@@ -48,9 +49,8 @@ func DracoListener(address string, client *ethclient.Client, ch chan<- bool) {
 				log.Fatal(err)
 			}
 
-			fmt.Println(event)
-
 			if event.Name == "Mint" {
+				fmt.Println(event.Name)
 				result, err := contractABI.Unpack(event.Name, vLog.Data)
 				if err != nil {
 					log.Fatal(err)
@@ -106,6 +106,78 @@ func DracoListener(address string, client *ethclient.Client, ch chan<- bool) {
 				contractUpdateResult, err := r.ColContract.UpdateOne(context.TODO(), contractFilter, contractUpdate)
 
 				fmt.Println("user", userUpdateResult.ModifiedCount, "contract", contractUpdateResult.ModifiedCount)
+			}
+
+			if event.Name == "CustomTransfer" {
+				fmt.Println(event.Name)
+
+				result, err := contractABI.Unpack(event.Name, vLog.Data)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				from := fmt.Sprintf("%v", result[0])
+				to := fmt.Sprintf("%v", result[1])
+				amount := fmt.Sprintf("%v", result[2])
+
+				transaction := dom.Transaction{
+					TxHash:          vLog.TxHash.Hex(),
+					ContractAddress: vLog.Address.String(),
+					TxType:          event.Name,
+					From:            from,
+					To:              to,
+					Amount:          amount,
+					CreatedAt:       "",
+				}
+
+				r, err := model.NewModel()
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				var fromUser entity.User
+				fromUserFilter := bson.D{{Key: "address", Value: from}}
+				r.ColUser.FindOne(context.TODO(), fromUserFilter).Decode(&fromUser)
+
+				var toUser entity.User
+				toUserFilter := bson.D{{Key: "address", Value: to}}
+				r.ColUser.FindOne(context.TODO(), toUserFilter).Decode(&toUser)
+
+				zeroObjectId, _ := primitive.ObjectIDFromHex("000000000000000000000000")
+
+				if zeroObjectId != fromUser.ID {
+					updateAmount := new(big.Int)
+					biUserAmount := new(big.Int)
+					userAmount, _ := biUserAmount.SetString(fromUser.CreditAmount, 10)
+					biAmount := new(big.Int)
+					Amount, _ := biAmount.SetString(amount, 10)
+
+					updateAmount.Sub(userAmount, Amount)
+
+					update := bson.M{
+						"$set":  bson.M{"draco_amount": updateAmount.String()},
+						"$push": bson.M{"transactions": transaction},
+					}
+
+					r.ColUser.UpdateOne(context.TODO(), fromUserFilter, update)
+				}
+
+				if zeroObjectId != toUser.ID {
+					updateAmount := new(big.Int)
+					biUserAmount := new(big.Int)
+					userAmount, _ := biUserAmount.SetString(toUser.CreditAmount, 10)
+					biAmount := new(big.Int)
+					Amount, _ := biAmount.SetString(amount, 10)
+
+					updateAmount.Add(userAmount, Amount)
+
+					update := bson.M{
+						"$set":  bson.M{"draco_amount": updateAmount.String()},
+						"$push": bson.M{"transactions": transaction},
+					}
+
+					r.ColUser.UpdateOne(context.TODO(), toUserFilter, update)
+				}
 			}
 		}
 	}
