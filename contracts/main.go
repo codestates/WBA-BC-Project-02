53ebd1821a411a26d036446a2f1a2fcd9238f853
96sunghyun <visioner2168@gmail.com>
2023-01-17 18:20:25 +0900

    merging

--- contracts/main.go
+++ contracts/main.go
@@ -1,64 +1,95 @@
 package main
 
+import (
+	"context"
+	"crypto/ecdsa"
+	"encoding/hex"
+	"fmt"
+	"math/big"
+	"log"
 
-func GenerateRawTransactionData() {
-	mulsigAddr := ""
-	toAddr := ""
-	tokenAddr := common.HexToAddress("0xbc9F814D03f46B3D10C22e7490aC923de121d6A2")
-	tokenAmount := ""
+	// "github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+	// "github.com/chenzhijie/go-web3"
+	// "github.com/ethereum/go-ethereum/rlp"
+	"golang.org/x/crypto/sha3"
+)
 
-	client, err := ethclient.Dial("https://api.test.wemix.com")
+
+func SubmitTrasaction(dData []byte) {
+	// 첫번째 계정의 privateKey를 통한 address 도출
+	firstPk, err := crypto.HexToECDSA("9734a6fab2886cdcc017c9441790681fdf99dbdbe16da22506d070a66f28755f")
+	if err != nil {
+		log.Fatal(err)
+	}
+	firstPublicKey := firstPk.Public()
+    firstPublicKeyECDSA, ok := firstPublicKey.(*ecdsa.PublicKey)
+    if !ok {
+        fmt.Println("fail convert, publickey")
+    }
+    firstAddress := crypto.PubkeyToAddress(*firstPublicKeyECDSA)
+	// multisig contract의 주소
+	multisigAddr := common.HexToAddress("0xb80cE90781B7ed255695016fBcdd8389453ddA89")
+	// 실행하고자 하는 컨트랙트의 주소
+	destinationContractAddr := common.HexToAddress("0x7Ad6c6EF72e31FD8D7f5d2F584de1dcD3DC4769E")
+
+	// server url과 연동
+	client, err := ethclient.Dial("http://127.0.0.1:7545")
 	if err != nil {
 		fmt.Println("client error")
 	}
 
-	
+	// firstAddress의 nonce값 추출
+	nonce, err := client.PendingNonceAt(context.Background(), firstAddress)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	pk, err := crypto.HexToECDSA("c04e38b264d4bf35625090e4764c912718f4c8be1bce5c3c0796365f9732d0b0")
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	publicKey := pk.PublicKey
-	fromAddress := crypto.PubkeyToAddress(publicKey)
-
-	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	value := big.NewInt(int64(amount.(float64)) * 1000000000000000000)
+	// gasPrice 설정
 	gasPrice, err := client.SuggestGasPrice(context.Background())
 	if err != nil {
 		fmt.Println(err)
 	}
-	transferFnSignature := []byte("transfer(address,uint256)")
+	gasLimit := uint64(200000)
+
+	// 이녀석은 Tx를 보낼 때 전송할 value
+	zValue := big.NewInt(0)
+
+	// 실행할 함수의 signature 생성, methodID 생성
+	transferFnSignature := []byte("submitTransaction(address,uint256,bytes)")
 	hash := sha3.NewLegacyKeccak256()
 	hash.Write(transferFnSignature)
 	methodID := hash.Sum(nil)[:4]
 	fmt.Println(hexutil.Encode(methodID))
 
-	paddedAddress := common.LeftPadBytes(toAddress.Bytes(), 32)
-	paddedAmount := common.LeftPadBytes(value.Bytes(), 32)
-	zValue := big.NewInt(0)
+	// 아래는 인자로 넣을 data를 추가하는 과정
+	// Destination Address와 보낼 value, Destination에 도달해야 할 Transaction Data가 포함된다.
+	paddedAddr := common.LeftPadBytes(destinationContractAddr.Bytes(), 32)
+	paddedValue := common.LeftPadBytes(zValue.Bytes(), 32)
+	paddedData := common.LeftPadBytes(dData, 32)
+	fmt.Println(zValue.Bytes())
+	var data []byte
+	data = append(data, methodID...)
+	data = append(data, paddedAddr...)
+	data = append(data, paddedValue...)
+	data = append(data, paddedData...)
+	
+	// 인자값은 차례대로 계정논스, 멀티시그 컨트랙트 주소, 전송할 value, 가스제한, 가스비, 전송할 데이터
+	tx := types.NewTransaction(nonce, multisigAddr, zValue, gasLimit, gasPrice, data)
+	fmt.Println(nonce)
+	fmt.Println(multisigAddr)
+	fmt.Println(zValue)
+	fmt.Println(gasLimit)
+	fmt.Println(gasPrice)
+	fmt.Println(data)
 
-	var pdata []byte
-	pdata = append(pdata, methodID...)
-	pdata = append(pdata, paddedAddress...)
-	pdata = append(pdata, paddedAmount...)
+	chainID := big.NewInt(1337)
 
-	gasLimit := uint64(200000)
-
-	tx := types.NewTransaction(nonce, tokenAddr, zValue, gasLimit, gasPrice, pdata)
-	chainID, err := client.NetworkID(context.Background())
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), pk)
+	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), firstPk)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -67,12 +98,44 @@ func GenerateRawTransactionData() {
 	if err != nil {
 		fmt.Println(err)
 	}
-
-	c.JSON(200, gin.H{
-		"msg" : "success",
-	})
+	fmt.Println(tx)
+	fmt.Println(signedTx)
 }
 
-func main() {
+func GetDestinationfuncData() []byte {
+	// 실행할 함수의 signature 생성, methodID 생성
+	transferFnSignature := []byte("callMe(uint256)")
+	hash := sha3.NewLegacyKeccak256()
+	hash.Write(transferFnSignature)
+	methodID := hash.Sum(nil)[:4]
+	fmt.Println(hexutil.Encode(methodID))
 
+	// 아래는 인자로 넣을 data를 추가하는 과정
+	// Destination address에 함수 인자값으로 넣을 uint값
+	num := big.NewInt(339)
+	paddedNum := common.LeftPadBytes(num.Bytes(), 32)
+
+	var pdata []byte
+	pdata = append(pdata, methodID...)
+	pdata = append(pdata, paddedNum...)
+	fmt.Println("0x"+hex.EncodeToString(pdata))
+	return pdata
+}
+
+
+func main() {
+	// 두번째 계정의 privateKey를 통한 address 도출
+	// secondPk, err := crypto.HexToECDSA("30e9d002076fb2a3a10322738a62260a278073d6c42996b9f374032259cc8801")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// secondPublicKey := firstPk.Public()
+    // secondPublicKeyECDSA, ok := secondPublicKey.(*ecdsa.PublicKey)
+    // if !ok {
+    //     fmt.Println("fail convert, publickey")
+    // }
+    // secondAddress := crypto.PubkeyToAddress(*secondPublicKeyECDSA)
+
+	data := GetDestinationfuncData()
+	SubmitTrasaction(data)
 }
--- go.mod
+++ go.mod
@@ -3,15 +3,22 @@ module github.com/codestates/WBA-BC-Project-02
 go 1.19
 
 require (
+<<<<<<< HEAD
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/ethereum/go-ethereum v1.10.26
 	github.com/gin-gonic/gin v1.8.2
 	github.com/go-redis/redis/v9 v9.0.0-rc.2
 	github.com/gofrs/uuid v4.3.1+incompatible
+=======
+	github.com/chenzhijie/go-web3 v0.0.0-20221106065746-e008ed0e5419
+	github.com/ethereum/go-ethereum v1.10.26
+	github.com/gin-gonic/gin v1.8.2
+>>>>>>> 8a528a1 (fix: develop multisig)
 	github.com/joho/godotenv v1.4.0
 	github.com/miguelmota/go-ethereum-hdwallet v0.1.1
 	github.com/naoina/toml v0.1.2-0.20170918210437-9fafd6967416
 	go.mongodb.org/mongo-driver v1.11.1
+<<<<<<< HEAD
 	go.uber.org/zap v1.24.0
 	golang.org/x/crypto v0.5.0
 	golang.org/x/sync v0.1.0
@@ -44,10 +51,38 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
+=======
+	golang.org/x/crypto v0.5.0
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/btcsuite/btcd/btcec/v2 v2.3.2 // indirect
+	github.com/deckarep/golang-set v1.8.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.1.0 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-playground/validator/v10 v10.11.1 // indirect
+	github.com/go-stack/stack v1.8.1 // indirect
+	github.com/goccy/go-json v0.9.11 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/google/uuid v1.2.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/compress v1.15.14 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/montanaflynn/stats v0.7.0 // indirect
+>>>>>>> 8a528a1 (fix: develop multisig)
 	github.com/naoina/go-stringutil v0.1.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rjeczalik/notify v0.9.1 // indirect
+<<<<<<< HEAD
 	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
 	github.com/tklauser/go-sysconf v0.3.5 // indirect
 	github.com/tklauser/numcpus v0.2.2 // indirect
@@ -60,6 +95,21 @@ require (
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	golang.org/x/net v0.5.0 // indirect
+=======
+	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
+	github.com/tklauser/go-sysconf v0.3.11 // indirect
+	github.com/tklauser/numcpus v0.6.0 // indirect
+	github.com/ugorji/go/codec v1.2.7 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.41.0 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20201027041543-1326539a0a0a // indirect
+	github.com/yusufpapurcu/wmi v1.2.2 // indirect
+	golang.org/x/net v0.5.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+>>>>>>> 8a528a1 (fix: develop multisig)
 	golang.org/x/sys v0.4.0 // indirect
 	golang.org/x/text v0.6.0 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
