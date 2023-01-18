9b5dc0b00a22200d7f8b037b48eb720c71efb7f9
96sunghyun <visioner2168@gmail.com>
2023-01-18 21:44:28 +0900

    fix: toml

--- contracts/signerdaemon/listener/multisiglistener.go
+++ contracts/signerdaemon/listener/multisiglistener.go
@@ -28,6 +28,10 @@ func MultisigListener(
 	secondPk := configContract.SecondPrivateKey
 	address := configContract.MultiSigAddr
 	channelId := configContract.ChannelToken
+
+	fmt.Println("firstPk: ", firstPk)
+	fmt.Println("secondPk: ", secondPk)
+	fmt.Println("address: ", address)
 	
 
 	contractAddress := common.HexToAddress(address)
